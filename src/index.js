947601f77bf00ba0a10308d5ddc9818faaed1633
madison sorah <madisonsorah@gmail.com>
2021-11-07 20:29:52 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let form = document.getElementById("create-task-form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const textBox = document.getElementById("new-task-description")
+    buildToDo(textBox.value);
+    form.reset();
+  });
 });
+
+function buildToDo(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement("button");
+  btn.addEventListener('click',handleDelete);
+  btn.textContent = 'x';
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  document.getElementById("tasks").appendChild(li);
+
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
